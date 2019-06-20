d7694df0150edd4a32e09b753851d2cfbb7bed95
Fabiana Minutti <fabianarminutti@gmail.com>
2019-06-19 21:55:10 -0300

    Ajustes front

---
+++ USC.Restaurante.Front/USC.Restaurante.Front/ClientApp/src/app/voto/voto.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-voto',
+  templateUrl: './voto.component.html'
+})
+export class VotoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
